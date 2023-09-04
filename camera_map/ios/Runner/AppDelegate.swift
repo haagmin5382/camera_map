6f2e24124580b26b29923709f271f5b3339c22f0
haagmin5382 <81218474+haagmin5382@users.noreply.github.com>
2023-09-04 18:42:45 +0900

    :sparkles: feat : add google map

--- camera_map/ios/Runner/AppDelegate.swift
+++ camera_map/ios/Runner/AppDelegate.swift
@@ -1,5 +1,19 @@
+// import UIKit
+// import Flutter
+
+// @UIApplicationMain
+// @objc class AppDelegate: FlutterAppDelegate {
+//   override func application(
+//     _ application: UIApplication,
+//     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+//   ) -> Bool {
+//     GeneratedPluginRegistrant.register(with: self)
+//     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//   }
+// }
 import UIKit
 import Flutter
+import GoogleMaps  // Add this import
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +22,11 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+   let apiKey = ProcessInfo.processInfo.environment["GOOGLE_MAP_API_KEY"] ?? ""
+    // TODO: Add your Google Maps API key
+    GMSServices.provideAPIKey("apiKey")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
