bcc1c6ca4fc167719329dd9514ae0bedce6165ba
haagmin5382 <81218474+haagmin5382@users.noreply.github.com>
2023-09-07 20:39:27 +0900

    :heavy_plus_sign: add firebase setting files

--- camera_map/macos/Flutter/GeneratedPluginRegistrant.swift
+++ camera_map/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import firebase_core
 import geolocator_apple
 import location
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
 }
