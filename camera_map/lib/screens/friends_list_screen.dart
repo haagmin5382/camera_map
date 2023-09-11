cbc55a5fb57e6b2cae9b6cd820ae1aa595a94b37
haagmin5382 <81218474+haagmin5382@users.noreply.github.com>
2023-09-11 19:27:56 +0900

    :lipstick: style : add friends_list layout to classify users status

--- camera_map/lib/screens/friends_list_screen.dart
+++ camera_map/lib/screens/friends_list_screen.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 class FriendListScreen extends StatelessWidget {
   // 친구 데이터 리스트 (예시)
   final List<Friend> friends = [
-    Friend(name: '친구 1', status: '온라인', imageUrl: 'assets/friend1.png'),
-    Friend(name: '친구 2', status: '오프라인', imageUrl: 'assets/friend2.png'),
-    Friend(name: '친구 3', status: '온라인', imageUrl: 'assets/friend3.png'),
+    Friend(name: '친구 1', isOnline: true, imageUrl: 'assets/friend1.png'),
+    Friend(name: '친구 2', isOnline: false, imageUrl: 'assets/friend2.png'),
+    Friend(name: '친구 3', isOnline: true, imageUrl: 'assets/friend3.png'),
     // 추가 친구 데이터...
   ];
 
@@ -22,12 +22,29 @@ class FriendListScreen extends StatelessWidget {
         itemCount: friends.length,
         itemBuilder: (context, index) {
           final friend = friends[index];
+          final statusIcon = friend.isOnline
+              ? const Icon(
+                  Icons.circle,
+                  color: Colors.green,
+                  size: 10,
+                )
+              : const Icon(
+                  Icons.circle,
+                  color: Colors.grey,
+                  size: 10,
+                );
           return ListTile(
             leading: CircleAvatar(
               backgroundImage: AssetImage(friend.imageUrl),
             ),
-            title: Text(friend.name),
-            subtitle: Text(friend.status),
+            title: Row(
+              children: [
+                Text(friend.name),
+                const Spacer(), // 오른쪽에 공간 추가
+                statusIcon
+              ],
+            ),
+
             // 친구를 탭했을 때 추가 동작을 설정할 수 있습니다.
             onTap: () {
               // 친구를 탭했을 때 수행할 동작 추가
@@ -42,8 +59,8 @@ class FriendListScreen extends StatelessWidget {
 
 class Friend {
   final String name;
-  final String status;
+  final bool isOnline;
   final String imageUrl;
 
-  Friend({required this.name, required this.status, required this.imageUrl});
+  Friend({required this.name, required this.isOnline, required this.imageUrl});
 }
