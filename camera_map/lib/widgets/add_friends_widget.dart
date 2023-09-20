8cc212b80de97deef2acbd218c2510e530e52804
haagmin5382 <81218474+haagmin5382@users.noreply.github.com>
2023-09-20 18:21:17 +0900

    :lipstick: style : add friends modal layout

---
+++ camera_map/lib/widgets/add_friends_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class FriendAddModal extends StatelessWidget {
+  // 모달 다이얼로그에서 입력받을 이메일을 저장할 변수
+  final TextEditingController emailController = TextEditingController();
+  final FocusNode emailFocusNode = FocusNode();
+
+  FriendAddModal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('친구 추가'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(
+            controller: emailController,
+            focusNode: emailFocusNode,
+            decoration: const InputDecoration(
+              hintText: '이메일을 입력하세요',
+            ),
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              // 이메일을 사용하여 친구 추가 로직을 수행
+              // emailController.text 변수에 입력된 이메일 사용
+              print(emailController.text);
+              // ...
+              Navigator.of(context).pop(); // 모달 닫기
+            },
+            child: const Text('추가'),
+          ),
+        ],
+      ),
+    );
+  }
+}
